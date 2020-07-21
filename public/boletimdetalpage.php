d7c312b10e020faa5be493bcc1372d56d2f77471
dmb-dev87 <dmbdev800@gmail.com>
2020-07-21 11:00:25 +0300

    Modify directory structure

--- public/boletimdetalpage.php
+++ public/boletimdetalpage.php
@@ -6,12 +6,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="asset/css/boletim.css">
+  <link rel="stylesheet" href="../asset/css/boletim.css">
 </head>
 
 <body>
   <?php
-    include './src/boletimdetalhado.php';
+    include '../parser/boletimdetalhado.php';
     displayDetalPage();
   ?>
 
